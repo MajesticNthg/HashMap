d51ded271e72015b475248fd354cd81dc7d579f0
MajesticNthg <majestic_nthg@mail.ru>
2023-12-24 01:08:45 +1000

    mod.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,66 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.HashMap;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    static void list (int[] randNumber, int N) {
+        HashMap<Integer, Integer> finalNumbers = new HashMap<Integer, Integer>(); // итоговый словарь
+        int x = 0;
+        for(int i = 0; i < randNumber.length; i++) {
+            x = 0;
+            for(int y = 0; y < randNumber.length; y++) {
+                if (randNumber[i] == randNumber[y])
+                    x++;
+                if (x == N) {
+                    finalNumbers.put(randNumber[i], i); // поскольку ключ - уникальное значение, то оно автоматически убирает дубликаты
+                    break;
+                }
+            }
         }
+        for(Integer i : finalNumbers.keySet()) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] randNumber = new int[100];
+        for (int i = 0; i < 100; i++) {
+            randNumber[i] = rand.nextInt(10) + 1;
+        } // массив чисел для проверки
+        list(randNumber, 12); // функция для Задания 2
+
+        // Задание 1
+        String[] Rule = new String[4]; // массив строк
+        Rule[0] = "FirstRule";
+        Rule[1] = "SecondRule";
+        Rule[2] = "ThirdRule";
+        Rule[3] = "FourthRule";
+
+        int[] keys = new int[100]; // массив ключей
+        int i = 0;
+        while(i < 100) {
+            int randInt = rand.nextInt(500)+1;
+            boolean badRandom = false;
+            for (int j = 0; j < i; j++) {
+                if (keys[j] == randInt) {
+                    badRandom = true;
+                    break;
+                }
+            }
+            if (badRandom) continue;
+            keys[i] = randInt;
+            i++;
+        }
+
+        HashMap<Integer, String> randMap = new HashMap<Integer, String>();
+
+        for (int x = 0; x < keys.length; x++) {
+            randMap.put(keys[x], Rule[rand.nextInt(4)]);
+        } // словарь из 100 случайных пар
+
+        for (Integer y : randMap.keySet()) {
+            System.out.println("key " + y + " value: " + randMap.get(y));
+        }
+        randMap.clear();
+
     }
 }
